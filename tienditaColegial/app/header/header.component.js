81140c61fb3377e6696b75894ff55dfe5c46a19e
cristianrmelendez <cristianrmelendez@gmail.com>
2016-10-02 16:19:55 -0400

    Delete everything from the Phones template app

---
+++ tienditaColegial/app/header/header.component.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//Register 'sign-in' component alon with its associated controller
+
+angular
+  .module('header')
+  .component('header-nav', {
+    templateUrl: 'header/header.template.html',
+    controller: [ function signInController() {
+        var self = this;
+
+    }]
+});
