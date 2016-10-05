99340f07689f6a7dc422d8285a91f92ae41278e8
cristianrmelendez <cristianrmelendez@gmail.com>
2016-10-05 19:03:01 -0400

    Se routio todo el Angular Material

--- tienditaColegial/app/app.module.js
+++ tienditaColegial/app/app.module.js
@@ -15,4 +15,7 @@ angular.module('tienditaColegial', [
   'sign-in'
 
   // 'footer-nav'
-]);
+]).config(function ($mdThemingProvider) {
+          $mdThemingProvider.theme('default')
+            .primaryPalette('green')
+            .accentPalette('green');
