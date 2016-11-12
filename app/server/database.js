08ba1915f36a4c6dc115bae256550f8a87dd1daf
Luis J. Rodriguez <luis.rodriguez99@upr.edu>
2016-11-11 21:17:15 -0400

    adding pg

--- app/server/database.js
+++ app/server/database.js
@@ -1,45 +1,16 @@
-
 var pg = require('pg');
 
-// create a config to configure both pooling behavior
-// and client options
-// note: all config is optional and the environment variables
-// will be read if the config is not present
-var config = {
-    user: 'foo', //env var: PGUSER
-    database: 'my_db', //env var: PGDATABASE
-    password: 'secret', //env var: PGPASSWORD
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, //env var: PGPORT
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-};
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
 
-
-//this initializes a connection pool
-//it will keep idle connections open for a 30 seconds
-//and set a limit of maximum 10 idle clients
-var pool = new pg.Pool(config);
-
-// to run a query we can acquire a client from the pool,
-// run a query on the client, and then return the client to the pool
-pool.connect(function(err, client, done) {
-    if(err) {
-        return console.error('error fetching client from pool', err);
-    }
-    client.query('SELECT $1::int AS number', ['1'], function(err, result) {
-        //call `done()` to release the client back to the pool
-        done();
-
-        if(err) {
-            return console.error('error running query', err);
-        }
-        console.log(result.rows[0].number);
-        //output: 1
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
     });
 });
 
-pool.on('error', function (err, client) {
 
-    console.error('idle client error', err.message, err.stack)
-})
+module.exports = pg;
