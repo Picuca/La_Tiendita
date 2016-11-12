e9e25c96ec5bb893e4fbf9d6a6184b0e4eb7c448
Picuca <robergzmn@yahoo.com>
2016-11-12 19:31:53 -0400

    OK

--- app/server/client/components/account.component.js
+++ app/server/client/components/account.component.js
@@ -12,24 +12,16 @@ angular
           {"id":1 , "name": "admin ", "lastname": "admin", "username": "admin" , "password": "admin", "email": "admin@email.com" , "payMethod":"ATH", "phone": "787-XXX-XXX" },
           {"id":2 , "name": "Manuel", "lastname": "Rodriguez", "username": "manuel" , "password": "password", "email": "manuel@email.com" , "payMethod":"ATH", "phone": "787-XXX-XXX" }];
 
-
-
       $scope.submit = function () {
 
-          var req = {
-              method: 'POST',
-              url: 'http://tienditatest.herokuapp.com/account',
-              headers: {
-                'Content-Type': undefined
-              },
-              data: {
-                  username: $scope.username,
-                  password: $scope.password
-              }
-          }
 
-          $http(req).then(function(res) {
-              console.log(res.data);
+          $http({
+              method: 'GET',
+              url: 'http://localhost:3000/account',
+          }).then(function(res) {
+              $scope.users = res.data;
+
+              console.log($scope.users);
           }, function(err) {
               console.log(err);
           });
@@ -38,8 +30,8 @@ angular
                 if($scope.username == $scope.users[i].username && $scope.password == $scope.users[i].password){
                     $rootScope.loggedIn = true;
                     $rootScope.currentUser = $scope.users[i];
+                    console.log($rootScope.currentUser);
                     $rootScope.$broadcast('logged', {"message": "Mi cuenta", "showLogout": true});
-                    console.log($rootScope.currentUser)
                     $location.path('account-info')
 
                 }else if($scope.username == null || $scope.password == null ){
