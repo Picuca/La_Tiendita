1f5ae229d7324959da30c0c8052ac1b43b0a1538
Luis J. Rodriguez <luis.rodriguez99@upr.edu>
2016-11-11 21:17:15 -0400

    adding pg

--- app/server/server.js
+++ app/server/server.js
@@ -8,7 +8,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-//  require('./database');
+require('./database');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'serverviews'));
