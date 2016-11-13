681ecad02513d4ba68522fec997a7159f26206bc
Picuca <robergzmn@yahoo.com>
2016-11-12 20:21:41 -0400

    OK

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -1,34 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var pg = require('../database');
+module.exports = function (app) {
 
-/* GET home page. */
-router.get('/account', function(req, res) {
-  pg.connect(function(err, client, done) {
-    if(err) {
-      return console.error('error fetching client from pool', err);
-    }
-    client.query('SELECT * FROM CUSTOMER', function(err, result) {
-      done();
 
-      if(err) {
-        return console.error('error running query', err);
-      }
-      res.send(result.rows[0]);
-      console.log(result.rows);
-    });
-  });
+  var homePage = require('./home-page');
+  app.get('/home-page', homePage.welcome);
 
-  pg.on('error', function (err, client) {
-    console.error('idle client error', err.message, err.stack)
-  });
+};
 
-});
-
-router.post('/account', function (req, res) {
-
-  console.log('POST PERFORMED');
-
-});
-
-module.exports = router;
