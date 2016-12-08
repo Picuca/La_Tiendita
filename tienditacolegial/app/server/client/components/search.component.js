b091369478f36d6ac859c5c3f090ebb627b9e4ac
giovanni gardon <giovanni.gardon@gmail.com>
2016-12-07 23:32:53 -0400

    search bar

--- tienditacolegial/app/server/client/components/search.component.js
+++ tienditacolegial/app/server/client/components/search.component.js
@@ -5,23 +5,35 @@ angular.
   component('search', {
     templateUrl: 'views/search.template.html',
 
-}).controller('searchController', function ($scope, $http) {
+}).controller('searchController', function ($scope, $http, itemSearchService, itemDetailService) {
 
+    $scope.itemName = '';
 
-
-
+                    var x = itemSearchService.get();
 
                      $http({
                             method: 'GET',
                             url:'http://localhost:3000/search',
-                            params:{ item: itemSearchService.get()}
+                            params:{p1: x}
+
                         }).then(function (res) {
                             console.log(res.data);
                             $scope.articulos = res.data;
                         }, function (err) {
                             console.log(err);
 
-                        })
+                        });
+
+                        $scope.getSearchDetails = function (ev,someShirt) {
+
+                        itemDetailService.setItemDetails(someShirt);
+                            itemDetailService.showItemDetails(ev);
+                          };
+
+
+    $scope.selectedSize = '';
+    $scope.itemSizes = ['10 - 12','14 - 16','Pequeña','Mediana','Grade','Extra Grande','Extra Extra Grande'];
+
 
 
 
